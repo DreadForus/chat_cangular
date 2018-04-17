4608dfe3ffab9ea7ecbb7b303f5fcb57caa96ea3
petrossi <edipetrossi@gmail.com>
2018-04-17 23:57:43 +0300

    inti

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,12 +3,10 @@ import { RouterModule } from '@angular/router';
 import { MatSidenavModule } from '@angular/material';
 
 import { FuseSharedModule } from '@fuse/shared.module';
-import { FuseNavigationModule, FuseSearchBarModule, FuseShortcutsModule, FuseSidebarModule, FuseThemeOptionsModule } from '@fuse/components';
+import { FuseNavigationModule, FuseSearchBarModule, FuseShortcutsModule } from '@fuse/components';
 
 import { FuseContentModule } from 'app/main/content/content.module';
-import { FuseFooterModule } from 'app/main/footer/footer.module';
 import { FuseNavbarModule } from 'app/main/navbar/navbar.module';
-import { FuseQuickPanelModule } from 'app/main/quick-panel/quick-panel.module';
 import { FuseToolbarModule } from 'app/main/toolbar/toolbar.module';
 
 import { FuseMainComponent } from './main.component';
@@ -25,16 +23,12 @@ import { FuseMainComponent } from './main.component';
 
         FuseSharedModule,
 
-        FuseThemeOptionsModule,
         FuseNavigationModule,
         FuseSearchBarModule,
         FuseShortcutsModule,
-        FuseSidebarModule,
 
         FuseContentModule,
-        FuseFooterModule,
         FuseNavbarModule,
-        FuseQuickPanelModule,
         FuseToolbarModule,
     ],
     exports     : [
